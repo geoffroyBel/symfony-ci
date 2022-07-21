6f2b12296370883cfeb1b38889591f6412b652b6
geoff bell <rrastakong@gmail.com>
2022-07-21 04:04:52 +0200

    trusted proxies

--- public/index.php
+++ public/index.php
@@ -6,9 +6,10 @@ use Symfony\Component\HttpFoundation\Request;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    $trustedProxies = $context['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+    
+    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
 $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if($context['APP_ENV'] == 'prod') $trustedProxies[] = $context['REMOTE_ADDR'];
+if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
 if($trustedProxies) {
     Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
