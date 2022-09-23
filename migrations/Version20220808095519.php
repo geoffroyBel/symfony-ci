2dbae93e2b0045839b764838ad7989ab8a3c4e36
geoff bell <rrastakong@gmail.com>
2022-09-23 16:45:27 +0200

    migration

--- migrations/Version20220808095519.php
+++ migrations/Version20220808095519.php
@@ -20,16 +20,16 @@ final class Version20220808095519 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE prestation ADD owner_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE prestation ADD CONSTRAINT FK_51C88FAD7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_51C88FAD7E3C61F9 ON prestation (owner_id)');
+       // $this->addSql('ALTER TABLE prestation ADD owner_id INT DEFAULT NULL');
+       // $this->addSql('ALTER TABLE prestation ADD CONSTRAINT FK_51C88FAD7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
+       // $this->addSql('CREATE INDEX IDX_51C88FAD7E3C61F9 ON prestation (owner_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE prestation DROP FOREIGN KEY FK_51C88FAD7E3C61F9');
-        $this->addSql('DROP INDEX IDX_51C88FAD7E3C61F9 ON prestation');
-        $this->addSql('ALTER TABLE prestation DROP owner_id');
+       // $this->addSql('ALTER TABLE prestation DROP FOREIGN KEY FK_51C88FAD7E3C61F9');
+        //$this->addSql('DROP INDEX IDX_51C88FAD7E3C61F9 ON prestation');
+        //$this->addSql('ALTER TABLE prestation DROP owner_id');
     }
 }
