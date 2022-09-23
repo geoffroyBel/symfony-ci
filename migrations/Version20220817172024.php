1a13d6402ac5d4ef1c47570f957fa9408f583bdb
geoff bell <rrastakong@gmail.com>
2022-09-23 16:17:52 +0200

    migration&repo

--- migrations/Version20220817172024.php
+++ migrations/Version20220817172024.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220719011250 extends AbstractMigration
+final class Version20220817172024 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220719011250 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        //$this->addSql('DROP TABLE prestation');
+        $this->addSql('ALTER TABLE user CHANGE enabled enabled TINYINT(1) DEFAULT 0');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        //$this->addSql('CREATE TABLE prestation (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE user CHANGE enabled enabled TINYINT(1) DEFAULT NULL');
     }
 }
